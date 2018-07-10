323736115c3729d78f10b25f9d4fb600a24108ba
LuYi <luyi985@gmail.com>
2018-07-11 09:20:25 +1000

    install matplotlib

--- play-tensorflow/main.py
+++ play-tensorflow/main.py
@@ -1,1 +1,10 @@
-import tensorflow as tfhello = tf.constant('Hello, TensorFlow!')sess = tf.Session()print(sess.run(hello))
+import pandas as pd
+print(pd.__version__)
+# city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+# population = pd.Series([852469, 1015785])
+
+# print(pd.DataFrame({ 'City name': city_names, 'Population': population }))
+california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+print(california_housing_dataframe.describe())
+print(california_housing_dataframe.head())
+california_housing_dataframe.hist('housing_median_age')
